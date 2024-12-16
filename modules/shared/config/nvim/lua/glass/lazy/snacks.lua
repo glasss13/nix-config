89f252ee01f6a42868899444a8ba88306974b93d
Max Glass <glassmax13@gmail.com>
2024-12-16 15:25:39 -0500

    snacks

---
+++ modules/shared/config/nvim/lua/glass/lazy/snacks.lua
@@ -0,0 +1,33 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        indent = { enabled = true, animate = { enabled = false } },
+        input = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        statuscolumn = { enabled = true },
+        terminal = {
+            win = {
+                position = "float",
+                width = 0.8,
+                height = 0.6,
+            },
+        },
+    },
+
+    keys = {
+        { "<leader>gb", function() Snacks.git.blame_line() end,   desc = "Git Blame Line" },
+        { "<leader>gB", function() Snacks.gitbrowse() end,        desc = "Git Browse" },
+        { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+        { "<leader>gg", function() Snacks.lazygit() end,          desc = "Lazygit" },
+        { "<leader>gl", function() Snacks.lazygit.log() end,      desc = "Lazygit Log (cwd)" },
+        { "<c-/>",      function() Snacks.terminal() end,         desc = "Toggle Terminal" },
+        { "<c-_>",      function() Snacks.terminal() end,         desc = "which_key_ignore" },
+    },
+
+}
--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -16,6 +16,7 @@ with pkgs; [
   fzf
   go
   go-task
+  lazygit
   neofetch
   ninja
   opam
