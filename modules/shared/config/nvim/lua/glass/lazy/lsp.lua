fc88d773d6050e3c317e3a55c8c09be932d3e796
Max Glass <glassmax13@gmail.com>
2024-12-17 02:51:53 -0500

    Use blink.cmp

--- modules/shared/config/nvim/lua/glass/lazy/lsp.lua
+++ modules/shared/config/nvim/lua/glass/lazy/lsp.lua
@@ -1,85 +1,74 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason.nvim",
+        { "williamboman/mason.nvim", config = true },
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+        "saghen/blink.cmp",
     },
     config = function()
+        local lsp_servers = {
+            lua_ls = {},
+            rust_analyzer = {},
+            ts_ls = {},
+            clangd = {},
+            gopls = {},
+            pylsp = {},
+            rnix = {},
+        }
+
+        local ensure_installed = {}
+        for server, _ in pairs(lsp_servers) do
+            table.insert(ensure_installed, server)
+        end
+
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "ts_ls",
-                "clangd",
-                "gopls",
-                "pylsp",
-                "rnix",
-            },
+            ensure_installed = ensure_installed,
             automatic_installation = true,
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup {}
-                end,
-                ["clangd"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.clangd.setup {
-                        cmd = {
-                            "clangd",
-                            "--background-index",
-                            "-j=12",
-                            "--clang-tidy",
-                            "--all-scopes-completion",
-                            "--completion-style=detailed",
-                            "--header-insertion-decorators",
-                            "--header-insertion=iwyu",
-                            "--pch-storage=memory",
-                        }
+        })
+
+
+        local lspconfig = require('lspconfig')
+
+        lspconfig["lua_ls"].setup({
+            settings = {
+                Lua = {
+                    runtime = { version = "Lua 5.1" },
+                    diagnostics = {
+                        globals = { "vim" }
                     }
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    }
-                end
+                }
             }
         })
 
-        local cmp = require("cmp")
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
+        lspconfig["clangd"].setup({
+            cmd = {
+                "clangd",
+                "--background-index",
+                "-j=12",
+                "--clang-tidy",
+                "--clang-tidy-checks=*",
+                "--suggest-missing-includes",
+                "--all-scopes-completion",
+                "--cross-file-rename",
+                "--completion-style=detailed",
+                "--header-insertion-decorators",
+                "--header-insertion=iwyu",
+                "--pch-storage=memory",
             },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-space>'] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'vsnip' },
-            }, {
-                { name = 'buffer' },
-            })
+            init_options = {
+                clangdFileStatus = true,
+                usePlaceholders = true,
+                completeUnimported = true,
+                semanticHighlighting = true,
+            },
         })
+
+        for server, config in pairs(lsp_servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
     end
 }
