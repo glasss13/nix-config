c4b279a926da8dbdd7e0409d61a857f9b4c395a3
Max Glass <glassmax13@gmail.com>
2024-12-25 14:31:26 -0500

    update blink

--- modules/shared/config/nvim/lua/glass/lazy/blink.lua
+++ modules/shared/config/nvim/lua/glass/lazy/blink.lua
@@ -9,8 +9,14 @@ return {
         keymap = { preset = 'default' },
 
         appearance = {
-            use_nvim_cmp_as_default = true,
+            use_nvim_cmp_as_default = false,
             nerd_font_variant = 'mono'
+        },
+
+        completion = {
+            documentation = {
+                treesitter_highlighting = true,
+            },
         },
 
         sources = {
